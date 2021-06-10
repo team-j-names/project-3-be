4d815c1e0f9c5edd6ca241bc8ddaa3777fffe794
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-06-10 09:20:14 -0700

    Added seeds.js and able to view data in browser with GET request

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const mongoURI =
-    process.env.NODE_ENV === 'production'
-    ? process.env.DB_URL
-    : 'mongodb://localhost/project-3-be'
+	process.env.NODE_ENV === 'production'
+		? process.env.DB_URL
+		: 'mongodb://localhost/project-3-be';
 
 mongoose
-    .connect(mongoURI, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    })
-    .then((instance) =>
-        console.log(`Connected to db: ${instance.connections[0].name}`)
-    )
-    .catch(console.error)
+	.connect(mongoURI, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then((instance) =>
+		console.log(`Connected to db: ${instance.connections[0].name}`)
+	)
+	.catch(console.error);
 
-module.exports = mongoose
+module.exports = mongoose;
