450c88e8d535640a6331257119df7362953da737
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-06-14 15:48:05 -0700

    Updated index.js and projects.js

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+const requestLogger = require('./middleware/request_logger');
+app.use(requestLogger);
+
 const projectController = require('./controllers/projects');
 app.use('/projects', projectController);
 
 const userController = require('./controllers/users');
 app.use('/api', userController);
+
+const { handleErrors } = require('./middleware/custom_errors');
+app.use(handleErrors);
 
 const port = process.env.PORT || 3000;
 
